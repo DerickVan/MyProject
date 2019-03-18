21561bcd96bb60bbd785310fc5ea76f475e366c2
Derick Vandekoken da Silva Nunes <kciredl@hotmail.com>
2019-03-18 17:39:11 -0300

    Revision

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # Memory Game Project
 
 ## Table of Contents
-
+* [Installation](#installation)
 * [Instructions](#instructions)
 * [Contributing](#contributing)
 
+## Installation
+
+Clone the GitHub repository to install.
+
+$ git clone http://github.com/
+
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+Open this index.html and play.
 
 ## Contributing
 
--- js/app.js
+++ js/app.js
@@ -11,6 +11,10 @@
  */
 
 // Shuffle function from http://stackoverflow.com/a/
+var moves =0;
+var block = false;
+var temporizador = 0;
+var inicioTemp= false;
 
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -26,106 +30,133 @@ function shuffle(array) {
     return array;
 }
 function createArray(array){
-   var arryElem = [
-                'fa fa-diamond',
-         		'fa fa-paper-plane-o',
-           		'fa fa-anchor',
-                'fa fa-bolt',
-                'fa fa-cube',
-                'fa fa-anchor', 
-               	'fa fa-leaf',
-                'fa fa-bicycle',
-                'fa fa-diamond',
-                'fa fa-bomb',
-                'fa fa-leaf',
-                'fa fa-bomb',
-               	'fa fa-bolt',
-                'fa fa-bicycle',
-                'fa fa-paper-plane-o',
-                'fa fa-cube'
-            ];
-    arryElem = shuffle(arryElem);
-    array = arryElem;
+  let cards = [
+  "fa fa-diamond", 
+  "fa fa-paper-plane-o", 
+  "fa fa-anchor", 
+  "fa fa-bolt", 
+  "fa fa-cube", 
+  "fa fa-leaf", 
+  "fa fa-bicycle", 
+  "fa fa-bomb"]; // save all the initial symbols into one array.
+	let arryElem = [...cards, ...cards]; 
+ 	arryElem = shuffle(arryElem);
+	array = arryElem;
     return array;
+
 }
 
 function prepareGame(){
    $(this).addClass(array.pop());
 }
 function removeGame(){
-	$(this).removeClass();
+	$(this).removeClass();	
 }
+function effect(select1,select2,boolean){
+ 	if(boolean){
+ 		$(select1).effect("bounce",500);
+		$(select2).effect("bounce",500);
+		
+		setTimeout(function(){
+        	select1.removeClass('open show');
+			select2.removeClass('open show');
+		},400);
+ 	}else{
+ 		$('.open').effect("shake",500);
+		$('.open').css('background-color','red');
+
+		setTimeout(function(){
+        	select1.removeClass('open show');
+			select2.removeClass('open show');
+		},400);
+ 	}
+}
+function starsProcess(moves){
+	if(moves == 20){
+		$('.stars').children().last().remove();
+	}
+	if(moves == 28){
+		$('.stars').children().last().remove();	
+	}
+}
+function compareSelects(select1,select2){
+	if(select1.children().hasClass(select2.children().attr('class'))){
+		select1.addClass('match');
+		select2.addClass('match');
+		effect(select1,select2,true);
+	}else{
+		effect(select1,select2,false);
+	}
+	moves++;
+	starsProcess(moves);
+	$('.moves').text(moves);
+}
+
+var timer =	 setInterval(function(){
+	block = false;
+}, 1000);
+
 var array = createArray();
 
 $('.card i').each(prepareGame);
-
-var moves =0;
-var block = false;
 $('.moves').text('0');
 
 
 $('.card').on('click',function(){
+	if(!inicioTemp){
+		temporizador = Date.now();
+		inicioTemp=true;
+	}
 	if(!$(this).hasClass('match') && !block){
 		var card = $(this).addClass('open show');
 		var val =  $('.open').val('n').length;
-		
 		if(val >= 2){
-			block = true;
+			block=true;
 			var select1 = $('.open').first();
 			var select2 = $('.open').last();
-			if(select1.children().hasClass(select2.children().attr('class'))){
-				$(select1).addClass('match');
-				$(select2).addClass('match');
-				$(select1).effect("bounce",500);
-				$(select2).effect("bounce",500);
-				
-				setTimeout(function(){
-	        	$(select1).removeClass('open show');
-				$(select2).removeClass('open show');
-				},400);
-				
-			}else{
-				$('.open').effect("shake",500);
-				$('.open').css('background-color','red');
-
-				setTimeout(function(){
-		        	$(select1).removeClass('open show');
-					$(select2).removeClass('open show');
-				},400);
-
+			compareSelects(select1,select2);
 			}
-			moves++;
-			$('.moves').text(moves);
-
 		}
-		var timer =	 setInterval(function(){
-			block = false;
-		}, 1000);
-	}
+	});
 
-});
 $('.restart').on('click',function(){
 	console.log('restart');
 	$('.card').removeClass('open show match');
-	moves = 0;
 	$('.moves').text(0);
 	$('.card i').each(removeGame);
 	array = createArray();
 	$('.card i').each(prepareGame);
-
+	inicioTemp = false;
+	if(moves >= 20){
+	var elem ='<li><i class="fa fa-star"></i></li>';
+	}
+	if(moves >= 26){
+	elem = + elem +' <li><i class="fa fa-star"></i></li>';
+	}
+	$('.stars').append(elem);
+	moves = 0;
 });
 
 $('.card').on('click',function(){
 	var totalMatch = $('.match').val('n').length;
 	console.log(totalMatch);
-	if(totalMatch >= 16){
-		$('.modal').modal({clickClose:false});
+	if(totalMatch >= 2){
+		$('#myModal').modal('show');
+		console.log(temporizador);
+		console.log(Date.now());
+		temporizador = (Date.now() - temporizador)/1000;
+		$('.temp').text(temporizador);
+		$('.nstar').text($('.stars li').length);
+
 	}
 });
 // quebrei a cabeça mas nao sei porque nao fecha o modal pelo evt, somente aquele botao X fecha).
-$('.modal').on('click',function(){
-	$('.modal').modal('hide');
+$('#button1').on('click',function(){
+	$('#myModal').modal('hide');
+	console.log('foi clicado');
 });
+
+
 
 /*
  * set up the event listener for a card. If a card is clicked:
